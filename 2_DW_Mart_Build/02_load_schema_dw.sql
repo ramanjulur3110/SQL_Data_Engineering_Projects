75a2c674a56e7ee0b84a16dd029fc42d9d585797
Rakeh Ramanjulu <rraman3110@gmail.com>
2026-07-05 01:59:37 -0400

    data validation check sat

--- 2_DW_Mart_Build/02_load_schema_dw.sql
+++ 2_DW_Mart_Build/02_load_schema_dw.sql
@@ -32,3 +32,27 @@ SELECT '=== Loading skill_job_dim Table ===' as info;
 INSERT INTO skills_job_dim (skill_id, job_id)
     SELECT skill_id, job_id 
     FROM read_csv('https://storage.googleapis.com/sql_de/skills_job_dim.csv', AUTO_DETECT=true);
+
+
+--DATA VALIDATION--
+-- Verify data was loaded correctly
+SELECT 'Company Dim' as table_name, COUNT(*) as record_count from company_dim
+UNION ALL
+SELECT 'Skills Dim', COUNT(*) from skills_dim
+UNION ALL
+SELECT 'Job Postings Fact', COUNT(*) from job_postings_fact
+UNION ALL
+SELECT 'Skills Job Dim', COUNT(*) from skills_job_dim;
+
+-- Show sample data
+SELECT '=== Company Dimension Sample ===' AS info;
+SELECT * FROM company_dim LIMIT 5;
+
+SELECT '=== Skills Dimension Sample ===' AS info;
+SELECT * FROM skills_dim LIMIT 5;
+
+SELECT '=== Job Postings Fact Sample ===' AS info;
+SELECT * FROM job_postings_fact LIMIT 5;
+
+SELECT '=== Skills Job Dim Sample ===' AS info;
+SELECT * FROM skills_job_dim LIMIT 5;
